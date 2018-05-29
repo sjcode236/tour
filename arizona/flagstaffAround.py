eb79873b5a407d94d908893025032ef3761d308c
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-05-29 13:19:10 -0700

    Create flagstaffAround.py

---
+++ arizona/flagstaffAround.py
@@ -0,0 +1,16 @@
+
+
+
+
+`Upper Lake Mary Near Flagstaff
+http://www.arizona-leisure.com/upper-lake-mary.html
+
+`Mormon Lake
+http://www.arizona-leisure.com/mormon-lake.html
+
+
+
+`Crescent Moon Picnic Area  Sedona 
+https://www.visitarizona.com/uniquely-az/parks-and-monuments/crescent-moon-picnic-area
+https://www.fs.usda.gov/recarea/coconino/recarea/?recid=55286
+
