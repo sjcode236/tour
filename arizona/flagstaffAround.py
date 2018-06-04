e47fa06bc753a3a6470723caeb838e173c4c1395
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-06-04 16:52:47 -0700

    Update flagstaffAround.py

--- arizona/flagstaffAround.py
+++ arizona/flagstaffAround.py
@@ -31,5 +31,15 @@ https://www.fs.usda.gov/recarea/coconino/recarea/?recid=55286
   visit  lookout mountain near  stoneman lake 
   
   
+  `========  prescot  ===============
+  watson lake
+  lynx lake
+    gold water lake 
+  http://www.arizona-leisure.com/lynx-lake.html
+  == http://www.mortimerfarmsaz.com
+    12907 E. State Route 169 ,Dewey, AZ 86327
+    
+    
+  
   
   
