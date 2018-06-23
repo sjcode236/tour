3f870656473ccf9b9bf1d2c5a90f6bb2eab877c8
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-06-23 12:07:19 -0700

    Create prescot.py

---
+++ arizona/prescot.py
@@ -0,0 +1,11 @@
+
+
+
+
+`========  prescot  ===============
+  watson lake
+  lynx lake
+    gold water lake 
+  http://www.arizona-leisure.com/lynx-lake.html
+  == http://www.mortimerfarmsaz.com
+    12907 E. State Route 169 ,Dewey, AZ 86327
