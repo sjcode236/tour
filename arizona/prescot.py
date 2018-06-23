f6675b8a48580b1d883cb743c5ce00afc5f2626d
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-06-23 12:14:04 -0700

    Update prescot.py

--- arizona/prescot.py
+++ arizona/prescot.py
@@ -9,3 +9,16 @@
   http://www.arizona-leisure.com/lynx-lake.html
   == http://www.mortimerfarmsaz.com
     12907 E. State Route 169 ,Dewey, AZ 86327
+    
+    
+    
+=======================================================
+    Heritage Park Zoological Sanctuary
+    1403 Heritage Park Road, Prescott, Arizona 86301
+    http://www.heritageparkzoo.org
+     http://www.heritageparkzoo.org/index.php/visiting/hours-admission
+    =WILDLIGHTS
+    =Santa with the Animals
+    =Kids get in FREE week: December 24 - 28, 2018
+=======================================================      
+    
