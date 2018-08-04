c80fff1dde5cb91e77ae3b49434eacf5efdd24db
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-08-04 11:28:19 -0700

    Update prescot.py

--- arizona/prescot.py
+++ arizona/prescot.py
@@ -5,12 +5,10 @@
 `========  prescot  ===============
   watson lake
   lynx lake
-    gold water lake 
+  gold water lake 
   http://www.arizona-leisure.com/lynx-lake.html
-  == http://www.mortimerfarmsaz.com
+ ==== http://www.mortimerfarmsaz.com=========
     12907 E. State Route 169 ,Dewey, AZ 86327
-    
-    
     
 =======================================================
     Heritage Park Zoological Sanctuary
@@ -20,5 +18,26 @@
     =WILDLIGHTS
     =Santa with the Animals
     =Kids get in FREE week: December 24 - 28, 2018
-=======================================================      
+=========================================================      
     
+
+
+====The Kayla's Hands Park ======
+1200 Commerce Dr, Prescott, AZ 86305
+   =kids play area 
+
+====FainPark    
+ 2205 N 5th St
+Prescott Valley, AZ 86314
+(its  near lynx  lake )
+
+
+====Acker Park  =====  
+ 421 S Virginia St, Prescott, AZ 86303
+  =1.5 miles  trains 
+  =on way to gold water lake 
+  
+
+====Granite Creek Park=====
+554 6th St, Prescott, AZ 86301
+   =heared not maintained 
