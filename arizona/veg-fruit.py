d5f20045eea05cb59388d1f50e8c5bf5eda039e8
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-08-04 18:25:14 -0700

    Create veg-fruit.py

---
+++ arizona/veg-fruit.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+========== you pick ==============
+
+
+
+=====https://www.datecreekranch.com/orchard    apple , peach picking 
+5 DATE CREEK RANCH RD , WICKENBURG, ARIZONA , 85358
+TEL: (928) 231-9058 Ryan
+TEL: (602) 527-9369 Savannah
