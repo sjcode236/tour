dadb56e05941438ff9a897d693d3c3b1a4c1657c
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-08-04 17:36:57 -0700

    Create sedona.py

---
+++ arizona/sedona.py
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+
+
+=====cornville is near  sedona=============================================
+there  is old  small country market  in cornville  and restaurant 
+
+====Folded Hills Orchard ===============
+1240 East Willow Point Road, Cornville, AZ, 86325
+(928) 634-4649
+  =peach picking
+  
+====http://www.oakcreekvineyards.net/
+1555 N Page Springs Rd, Cornville, AZ 86325
+928) 649-0290
+  =Tasting room
+  =open daily from 10 a.m. to 6 p.m. with Vino (think Happy) Hours between 6 and 8 p.m. Friday and Saturday evenings
+  =Our ten-acre, high desert estate vineyard sits at a 3,400-foot elevation and borders Oak Creek, which runs along Page Springs Road, home to our winery.
+
+====The Vineyards Bed and Breakfast 
+http://thevineyardsbandb.com/tiki-tree-house/
+1350 N Page Springs Rd, Cornville, AZ 86325
+(928) 300-4313
+
+
+====SYCAMORE SPRINGS  cottage rental   175 , 275  ====
+http://www.ssguesthouse.com/ranch-house.html
+470 S. Merritt Ranch Rd, Cornville AZ 86325 
+Shirley at  1-928-300-6813
+
+
+
+====Up The Creek Bistro - Wine Bar (restaurant) ==========
+https://www.upthecreekaz.com
+1975 North Page Springs Road,Cornville, AZ  86325
+ 928.634.9954
+ 
+ ====HARRY'S HIDEAWAY
+Quality Food + Reasonable Prices = Great Value!
+ 10990 E. Cornville Road, Ste C , Cornville, AZ 86325
+ Dinner Only Tuesday-Saturday: 5:30PM–7:30PM
+ ====G’s Burgers
+ https://www.facebook.com/gsburgersaz/menu/
+ 
+ 
