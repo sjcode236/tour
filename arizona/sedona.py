c77a9821dca0758de22ba676de08cc1d4f66b17a
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-08-04 18:21:32 -0700

    Update sedona.py

--- arizona/sedona.py
+++ arizona/sedona.py
@@ -14,7 +14,10 @@ there  is old  small country market  in cornville  and restaurant
 ====Folded Hills Orchard ===============
 1240 East Willow Point Road, Cornville, AZ, 86325
 (928) 634-4649
-  =peach picking
+  =peach picking , apples , blackberries 
+  
+==== peach and apricot picking on pagesprings roead 
+  =There is one you pick  peach , apricot  place on pagesprings road
   
 ====http://www.oakcreekvineyards.net/
 1555 N Page Springs Rd, Cornville, AZ 86325
@@ -23,16 +26,31 @@ there  is old  small country market  in cornville  and restaurant
   =open daily from 10 a.m. to 6 p.m. with Vino (think Happy) Hours between 6 and 8 p.m. Friday and Saturday evenings
   =Our ten-acre, high desert estate vineyard sits at a 3,400-foot elevation and borders Oak Creek, which runs along Page Springs Road, home to our winery.
 
-====The Vineyards Bed and Breakfast 
+====The Vineyards Bed and Breakfast ,  cottage too
 http://thevineyardsbandb.com/tiki-tree-house/
 1350 N Page Springs Rd, Cornville, AZ 86325
 (928) 300-4313
 
+====Page Springs Hatchery
+1600 N Page Springs Rd, Cornville, AZ 86325
+(928) 634-4805
+
+====Bubbling Ponds Fish Hatchery =======
+1970 N Page Springs Rd, Cornville, AZ 86325
+(928) 649-1297
+
+====Lo-Lo-Mai Springs Campground ======
+lolomai.com
+11505 E Lolo Mai Rd, Cornville, AZ 86325
+(928) 634-4700
 
 ====SYCAMORE SPRINGS  cottage rental   175 , 275  ====
 http://www.ssguesthouse.com/ranch-house.html
 470 S. Merritt Ranch Rd, Cornville AZ 86325 
 Shirley at  1-928-300-6813
+====kisstheskyaz  cottage ====
+10545 E Creekview Dr, Cornville, AZ 86325
+www.kisstheskyaz.com
 
 
 
