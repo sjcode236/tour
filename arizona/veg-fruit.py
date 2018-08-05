51fdeac10a5004a6d8a434619bfd56ec8382b511
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-08-04 18:39:06 -0700

    Update veg-fruit.py

--- arizona/veg-fruit.py
+++ arizona/veg-fruit.py
@@ -11,3 +11,8 @@
 5 DATE CREEK RANCH RD , WICKENBURG, ARIZONA , 85358
 TEL: (928) 231-9058 Ryan
 TEL: (602) 527-9369 Savannah
+
+  
+  
+Cornville, AZ Farms  
+https://www.yellowpages.com/cornville-az/farms
